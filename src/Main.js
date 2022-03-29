64dbd7b1db4405af7fcdbbbed17891725402e956
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2022-03-29 19:14:04 +0530

    lol

--- src/Main.js
+++ src/Main.js
@@ -17,8 +17,7 @@ function App() {
         <p>Web Application Developer with experience in creating. maintaining and testing source code for the front-end & back-end functionalities and also in 
         designing and developing application user interfaces, testing, and debugging. Experienced in optimizing front-end applications that result in improved data 
         retrieval and workflow efficiencies</p>
-        <br />
-        <button type='button' onClick={Main} >Press Me</button>
+        
       </div>
   );
 }
